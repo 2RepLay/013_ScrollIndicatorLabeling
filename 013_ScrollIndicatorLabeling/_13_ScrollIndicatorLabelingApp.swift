92fd90c066c816de403fa58a8bc74a1c0598efdd
Nikita Yankov <evne.yankovnikita@gmail.com>
2022-10-25 20:11:14 +0300

    Initial Commit

---
+++ 013_ScrollIndicatorLabeling/_13_ScrollIndicatorLabelingApp.swift
@@ -0,0 +1,17 @@
+//
+//  _13_ScrollIndicatorLabelingApp.swift
+//  013_ScrollIndicatorLabeling
+//
+//  Created by nikita on 25.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct _13_ScrollIndicatorLabelingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
